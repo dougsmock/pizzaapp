45b531f9dc2c7adac04b7a8747f801c2a2c8bbd5
dougsmock <39506285+dougsmock@users.noreply.github.com>
2018-06-17 11:12:43 -0400

    Add files via upload

--- pizzaapp.rb
+++ pizzaapp.rb
@@ -1,10 +1,8 @@
-# declaring some variables
-
 size = [10, 14, 18]
-crust = ["thin", "regular", "thick"]
-toppings = ["double cheese", "4 cheese", "onions", "mushrooms", "green peppers", "pepperoni", "sausage", "ham"]
+crust = ["Thin", "regular", "thick", "stuffed"]
+toppings = ["Double cheese", "4 cheese", "Onions", "Mushrooms", "Green peppers", "Pepperoni", "Sausage", "Ham"]
 
-#Delivery function. No seperate function on carryout, which is easier.
+#Delivery function. No seperate function on carryout, which is less involved.
 
 def delivery(price)
   puts "What is your address?"
@@ -15,6 +13,7 @@ def delivery(price)
   puts "Add a tip, if you wish."
   tip = gets.chomp.to_f
   price = price + (price * sales_tax) + delivery_charge + tip
+  price = price.round(2)
   puts "Your total will be \$#{price}, and"
   puts "your order will be to your door in 40 minutes.\n"
 end
@@ -28,45 +27,52 @@ puts "Welcome to Doug's Pizza. How many pizzas today?"
   i = 0
   total_price = 0.00
 
-while i < number_pizzas #We select each individual pizza.
+while i < number_pizzas #In large loop, we select each individual pizza.
   puts "Pizza No. #{i + 1}:"
   puts "What size would you like, 10, 14 or 18 inches?"
-  size = gets.chomp.to_i
+    size = gets.chomp.to_i
     if size == 10
       total_price = total_price + 4.99
     elsif size == 14
       total_price = total_price + 8.99
     elsif size == 18
       total_price = total_price + 12.99
+    else exit(0)
     end
 
-    puts "#{crust[0]}, #{crust[1]}, or #{crust[2]} crust?\n"
+    puts "#{crust[0]}, #{crust[1]}, #{crust[2]}, or #{crust[3]} crust?\n"
     crust_pick = gets.chomp.to_s
 
     top_price = 1.49
     tops_number = 0
     puts "What toppings would you like?:\n"
 
+    topping_array = []
+
     toppings.each do |topping|
-      #tops_selected = [] trying to push items into an array.
       puts "#{topping}? (y/n)\n"
       y_or_n = gets.chomp.to_s
         if y_or_n == "y"
-    #      tops_selected.push(topping)
+          topping_array.push(topping)
           tops_number += 1
-      end
-
+        end
     end
+
+    puts "Do you have any special requests?"
+    special_requests = gets.chomp.to_s
 
     total_price = total_price + (top_price * tops_number)
     puts "Pizza No. #{i + 1}"
     puts "#{size} inch"
     puts "#{crust_pick} crust"
-  #  puts tops_selected.each #How do I output arrays?
+      topping_array.each do |topping|
+        puts "#{topping}\n"
+      end
+    puts "#{special_requests}\n\n"
     puts "Subtotal: \$#{total_price}"
   i += 1
 
-end #end of big while loop, what stays in.
+end #end of large while loop.
 
 
 
@@ -77,11 +83,9 @@ puts "delivery or carryout? (type d or c)"
   elsif d_or_c == "c"
     sales_tax = 0.06
     total_price = total_price + (total_price * sales_tax)
+    total_price = total_price.round(2)
     puts "Your total will be \$#{total_price}"
     puts "It will be ready in 15 minutes."
   else
     puts "You may pick up your order in 15 minutes."
   end
-
-
-  #Another question: How do you round to two decimal points?
